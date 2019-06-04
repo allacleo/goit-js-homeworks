715d00d8bce5a22a8dcb71e7fe41f88307f85fda
Alla <alla.cleo@gmail.com>
2019-06-04 10:20:36 +0300

    hw-9 button_edited

--- module-08/js/ex.js
+++ module-08/js/ex.js
@@ -4,44 +4,31 @@ import quizData from './quiz-data.js';
 const form = document.querySelector('form');
 form.classList.add('form');
 
-// const button = document.querySelector('button');
-// button.classList.add('button');
-// button.setAttribute('type', 'submit');
-
-
-const button = document.createElement('button');
+const button = document.querySelector('button');
 button.classList.add('button');
 button.setAttribute('type', 'submit');
-button.textContent = "Проверить";
-
-
-
-
 
 const createQuizTest = function(form, quizData) {
   const formTitle = document.createElement('h2');
   formTitle.classList.add('form-title');
   formTitle.textContent = quizData.title;
 
-
-  
   // let sectionString = '';
 
-
-  quizData.questions.map((currentArrayQustions, currentQuestions) => {
+  quizData.questions.map((currentArrayQuestions, currentQuestions) => {
     const section = document.createElement('section');
     section.classList.add('form-sections');
     const sectionTitle = document.createElement('h3');
     sectionTitle.classList.add('form-sections__title');
     sectionTitle.textContent = `${currentQuestions + 1}. ${
-      currentArrayQustions.question
+      currentArrayQuestions.question
     }`;
     section.append(sectionTitle);
     const sectionList = document.createElement('ol');
     sectionList.classList.add('list');
     section.append(sectionList);
 
-    currentArrayQustions.choices.map((questions, currentAnswer) => {
+    currentArrayQuestions.choices.map((questions, currentAnswer) => {
       const quizLi = document.createElement('li');
       const quizLabel = document.createElement('label');
       quizLabel.textContent = questions;
@@ -56,16 +43,10 @@ const createQuizTest = function(form, quizData) {
       sectionList.append(quizLi);
     });
 
-
-
     // sectionString += section.outerHTML;
     form.append(section);
 
-
   });
-
-
-
   // form.insertAdjacentHTML('afterbegin', sectionString);
   form.prepend(formTitle);
   form.append(button);
@@ -87,8 +68,9 @@ function handlerSubmitForm(event) {
   });
 
   (countCorrectAnswer * 100) / questionsArray.length >= 80
-    ? (answer.textContent = 'Удача')
-    : (answer.textContent = 'Неудача');
+    ? (answer.textContent = 'Ты справился!')
+    : (answer.textContent = 'Ты потерпел неудачу:(');
   form.append(answer);
 }
+
 createQuizTest(form, quizData);
